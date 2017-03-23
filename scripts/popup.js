63f6c450020464d486bd83871c3173ff2f995e71
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2017-03-23 19:08:23 +0900

    Change to permission free architecture

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,12 +1,17 @@
 var buttons = document.getElementById('buttons');
 buttons.addEventListener('click', function (e) {
   ga('send', 'event', 'copy', 'click', e.target.id);
-  sendClick(e.target.id);
-  window.close();
+  sendClick(e.target.id, function () {
+    window.close();
+  });
 });
 
-function sendClick(formatType) {
+function sendClick(formatType, callback) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {format: formatType}, function(response) {});
+    chrome.tabs.executeScript(tabs[0].id, {
+      "file": "scripts/content.js"
+    }, function () {
+      chrome.tabs.sendMessage(tabs[0].id, {format: formatType}, callback);
+    });
   });
 }
